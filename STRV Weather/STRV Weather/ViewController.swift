e4ff8b9de7bb6e60049043ce87d0defda130a487
Wallace Souza <ssouza.wallace@gmail.com>
2019-01-15 13:19:35 -0200

    Empty project

---
+++ STRV Weather/STRV Weather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  STRV Weather
+//
+//  Created by Wallace Souza Silva on 15/01/19.
+//  Copyright © 2019 WallaCorp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
