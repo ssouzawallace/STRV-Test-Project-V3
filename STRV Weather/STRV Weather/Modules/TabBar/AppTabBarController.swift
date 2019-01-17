9d129f5e057409578d0eb6375c24d8048057dc62
Wallace Souza <ssouza.wallace@gmail.com>
2019-01-17 12:57:17 -0200

    tab bar items font
    tab bar item font and color

--- STRV Weather/STRV Weather/Modules/TabBar/AppTabBarController.swift
+++ STRV Weather/STRV Weather/Modules/TabBar/AppTabBarController.swift
@@ -6,13 +6,26 @@ class AppTabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        tabBar.barTintColor = .white
-        
         Locator.requestAuthorizationIfNeeded()
         
         LocationObserver.sharedInstance.startUpdating()
         
-        configureChildren()        
+        configureChildren()
+        configureStyle()
+    }
+    
+    func configureStyle() {
+        tabBar.barTintColor = .white
+        let normalTitleTextAttributes = [NSAttributedString.Key.font: UIFont.proximaNova(of: .semibold, withSize: 10),
+                                         NSAttributedString.Key.foregroundColor: UIColor(white: 51/255, alpha: 1)]
+        let selectedTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor(red: 47/255, green: 145/255, blue: 255/255, alpha: 1)]
+        children.forEach { child in
+            child.tabBarItem.setTitleTextAttributes(normalTitleTextAttributes,
+                                                    for: .normal)
+            
+            child.tabBarItem.setTitleTextAttributes(selectedTitleTextAttributes,
+                                                    for: .selected)
+        }
     }
     
     func configureChildren() {
